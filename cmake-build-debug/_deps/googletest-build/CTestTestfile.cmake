1da84c3b3c434c64df3e2a1be4b937b8db0d6f84
JackNLewis <jacklewis2809@gmail.com>
2022-04-09 11:14:24 +0100

    untracked build directory

--- cmake-build-debug/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jacklewis/Documents/work/year3/DMet/cmake-build-debug/_deps/googletest-src
-# Build directory: /Users/jacklewis/Documents/work/year3/DMet/cmake-build-debug/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jacklewis/Documents/work/year3/DMet/test
-# Build directory: /Users/jacklewis/Documents/work/year3/DMet/cmake-build-debug/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/jacklewis/Documents/work/year3/DMet/cmake-build-debug/test/Google_tests_run[1]_include.cmake")
-subdirs("../_deps/googletest-build")
